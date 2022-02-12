2c0515df83bf4468520d0d6013e62bf5c0f75385
joaoOliveira <joaoavelino2008@hotmail.com>
2022-02-12 20:52:29 -0300

    style(Footer): add some tailwind classes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,17 @@
 const Footer = () => {
   return (
-    <footer>
-        <p>João Oliveira</p>
+    <footer className="flex w-screen fixed inset-x-0 bottom-0 justify-center">
+      <div className="flex justify-between space-x-8 w-1/4 pb-36">
         <div>
+          <p>João Oliveira</p>
+        </div>
+        <div className="flex space-x-4">
           <p>Github</p>
           <p>Linkedin</p>
         </div>
+      </div>
     </footer>
   );
-}
- 
+};
+
 export default Footer;

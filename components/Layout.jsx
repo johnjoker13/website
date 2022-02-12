fdf4f3a37c7da80cdd72278bf3700403e142136b
joaoOliveira <joaoavelino2008@hotmail.com>
2022-02-12 20:01:27 -0300

    refactor(Layout.jsx): Add div to wrap content

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,10 +2,10 @@ import NavBar from "./NavBar";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className="min-h-screen">
       <NavBar />
       { children }
-    </>
+    </div>
   );
 }
  

a54655a9f9f3e7f7258a9f1c568547cd913e68d8
joaoOliveira <joaoavelino2008@hotmail.com>
2022-02-12 21:43:11 -0300

    style(NavBar): add style to nav

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,14 @@
 const Footer = () => {
   return (
-    <footer className="flex w-screen fixed inset-x-0 bottom-0 justify-center">
+    <footer className="flex fixed inset-x-0 bottom-0 justify-center w-screen">
       <div
         className="flex
-        justify-between
-        items-center
-        space-x-8
-        w-1/4
-        pb-36
-        font-Nunito"
+          justify-between
+          items-center
+          space-x-8
+          w-1/4
+          pb-36
+          font-Nunito"
       >
         <div className="text-lg">
           <p>João Oliveira</p>
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,18 +1,43 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <nav className="">
-      <div>
-        <Image
-          src="/profile-photo.jpg"
-          alt="foto-joao"
-          width={70}
-          height={40}
-        />
+    <nav className="flex sticky justify-center w-screen">
+      <div 
+        className="flex 
+          justify-between
+          items-center
+          space-x-6
+          w-3/5
+          p-3
+          border-b"
+      >
+        <div>
+          <Image
+            src="/profile-photo.jpg"
+            alt="foto-joao"
+            width={70}
+            height={40}
+          />
+        </div>
+
+        <div className="flex space-x-4 text-lg">
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+
+          <Link href="/projects">
+            <a>Projects</a>
+          </Link>
+
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </div>
       </div>
     </nav>
   );
-}
- 
+};
+
 export default NavBar;

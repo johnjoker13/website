a3df6c776b67113712c4818762ef42de80c01233
joaoOliveira <joaoavelino2008@hotmail.com>
2022-02-14 19:21:00 -0300

    fix(ThemeToggle): server/client rendering not equal

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -1,13 +1,15 @@
 import { FaSun, FaMoon } from "react-icons/fa";
 import { useContext } from "react";
 import AppContext from "../Context/AppContext";
+import { useLoaded } from "../Hooks/useLoaded";
 
 const Toggle = () => {
   const { theme, setTheme } = useContext(AppContext);
+  const loaded = useLoaded();
 
   return (
     <div className="transition duration-500 ease-in-out rounded-full p-2">
-      {theme === "dark" ? (
+      {loaded && theme === "dark" ? (
         <FaSun
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
           className="text-yellow-400 dark:text-yellow-400 text-2xl cursor-pointer"
